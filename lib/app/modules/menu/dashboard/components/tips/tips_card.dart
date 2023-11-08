1b5524e10d7ebb9692c3ac595ae193d0f112bb00
Fahad Hafizudin Dayana <89493279+fahadhafizdev@users.noreply.github.com>
2023-11-08 23:26:03 +0700

    Update tips_card.dart

---
+++ lib/app/modules/menu/dashboard/components/tips/tips_card.dart
@@ -0,0 +1,45 @@
+import 'package:cozy_v2/app/config/extension/extension.dart';
+import 'package:flutter/material.dart';
+
+class TipsCard extends StatelessWidget {
+  final Tips tips;
+  TipsCard(this.tips);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          tips.imageUrl,
+          width: 80,
+          height: 80,
+        ),
+        16.0.width,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              tips.title,
+              style: blackTextStyle.copyWith(
+                fontSize: 18,
+              ),
+            ),
+            Text(
+              'Updated ${tips.updatedAt}',
+              style: lightTextStyle.copyWith(
+                fontSize: 14,
+              ),
+            )
+          ],
+        ),
+        Spacer(),
+        IconButton(
+            icon: Icon(
+              Icons.chevron_right,
+              color: greyColor,
+            ),
+            onPressed: () {})
+      ],
+    );
+  }
+}
