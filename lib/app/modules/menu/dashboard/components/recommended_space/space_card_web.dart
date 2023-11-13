0c0ca228d0b2a7958b42478713d273ce2c4fded0
Fahad Hafizudin Dayana <89493279+fahadhafizdev@users.noreply.github.com>
2023-11-13 08:43:03 +0700

    Create space_card_web.dart

---
+++ lib/app/modules/menu/dashboard/components/recommended_space/space_card_web.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SpaceCardWeb extends StatelessWidget {
+  const SpaceCardWeb({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
