610e90e7f29bba4ec1827692f5b582cbe6bc3cc5
Fahad Hafizudin Dayana <89493279+fahadhafizdev@users.noreply.github.com>
2023-11-15 14:09:44 +0700

    Update main_page.dart

--- lib/app/modules/main_page/main_page.dart
+++ lib/app/modules/main_page/main_page.dart
@@ -3,6 +3,7 @@ import 'package:cozy_v2/app/data/src/image_string.dart';
 import 'package:cozy_v2/app/modules/menu/dashboard/dashboard_view.dart';
 import 'package:cozy_v2/app/modules/menu/favorite/favorite_view.dart';
 import 'package:cozy_v2/app/modules/menu/message/message_view.dart';
+import 'package:cozy_v2/app/modules/menu/news/news_view.dart';
 import 'package:cozy_v2/app/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -62,7 +63,7 @@ class _MainPageState extends State<MainPage> {
         case 1:
           return const MessageView();
         case 2:
-          return const FavoriteView();
+          return const NewsView();
         case 3:
           return const FavoriteView();
         default:
