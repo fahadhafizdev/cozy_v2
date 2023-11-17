4f3f766bf22694aa92adf1aac2c170d8173e62b0
Fahad Hafizudin Dayana <89493279+fahadhafizdev@users.noreply.github.com>
2023-11-17 14:51:49 +0700

    adjust view

--- lib/app/modules/menu/dashboard/components/header/dashboard_header.dart
+++ lib/app/modules/menu/dashboard/components/header/dashboard_header.dart
@@ -29,7 +29,7 @@ class DashboardHeader extends StatelessWidget {
       );
     }
 
-    Widget webView() {
+    Widget webViewLarge() {
       return Align(
         alignment: Alignment.topCenter,
         child: Padding(
@@ -142,10 +142,102 @@ class DashboardHeader extends StatelessWidget {
       );
     }
 
+    Widget webViewMedium() {
+      return Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 50),
+          child: Stack(
+            children: [
+              Container(
+                height: 600,
+                width: AppDimen.wInfinit,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [AppColor.mainColor, AppColor.orange],
+                    stops: const [0.25, 0.75],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(100),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(40, 30.h, 0, 0),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        ImageString.iconCozy,
+                        height: 72,
+                        width: 72,
+                      ),
+                      7.0.width,
+                      const TileNavigationWeb(title: 'dashboard'),
+                      7.0.width,
+                      const TileNavigationWeb(title: 'message'),
+                      7.0.width,
+                      const TileNavigationWeb(title: 'news'),
+                      7.0.width,
+                      const TileNavigationWeb(title: 'favorite'),
+                      7.0.width,
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(40, 180.h, 0, 0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Find Cozy House\nto Stay and Happy',
+                        textAlign: TextAlign.center,
+                        style: AppFont.whiteTextStyle.copyWith(fontSize: 35.sp),
+                      ),
+                      3.0.height,
+                      Text(
+                        'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                        textAlign: TextAlign.center,
+                        style: AppFont.whiteTextStyle.copyWith(
+                            fontSize: 20.sp,
+                            fontWeight: AppFont.light,
+                            color: AppColor.white.withOpacity(0.5)),
+                      ),
+                      SizedBox(
+                        height: 100,
+                        child: CustomButton(
+                          width: 100.w,
+                          margin: const EdgeInsets.only(top: 40),
+                          isLoading: false,
+                          func: () =>
+                              Navigator.pushNamed(context, '/main-page'),
+                          text: 'Explore Now',
+                          fontSize: 20.sp,
+                          btnStyle: AppButtonStyle.btnOrange,
+                          fontWeight: AppFont.medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return LayoutBuilder(
       builder: (context, constraint) {
-        if (constraint.maxWidth >= 1020) {
-          return webView();
+        if (constraint.maxWidth >= 1050) {
+          return webViewLarge();
+        } else if (constraint.maxWidth >= 700) {
+          return webViewMedium();
         } else {
           return mobileView();
         }
